af57aa7136dedbf4eab6aeccf664e933c5d2195a
Pedro <ferrarezzolabs@outlook.com>
2026-02-14 23:06:04 -0300

    fix: use @RegisterReflectionForBinding annotation to register jackson hints

--- notification/src/main/java/com/soat/fiap/videocore/notification/NotificationApplication.java
+++ notification/src/main/java/com/soat/fiap/videocore/notification/NotificationApplication.java
@@ -1,13 +1,16 @@
 package com.soat.fiap.videocore.notification;
 
 import com.soat.fiap.videocore.notification.common.hints.azure.BlobHints;
-import com.soat.fiap.videocore.notification.common.hints.jackson.JacksonHints;
+import com.soat.fiap.videocore.notification.infrastructure.in.event.azsvcbus.payload.ProcessVideoErrorPayload;
+import com.soat.fiap.videocore.notification.infrastructure.in.event.azsvcbus.payload.ProcessVideoStatusUpdatePayload;
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
-@ImportRuntimeHints({BlobHints.class, JacksonHints.class})
+@ImportRuntimeHints(BlobHints.class)
+@RegisterReflectionForBinding({ProcessVideoStatusUpdatePayload.class, ProcessVideoErrorPayload.class})
 public class NotificationApplication {
 
     static void main(String[] args) {
